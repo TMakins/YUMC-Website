3d076cd2cbf3c4f5b99927569e9c26a5930cd7db
Toby Makins <toby@makins.org.uk>
2016-06-12 21:45:00 +0100

    Dummy widgets for front page

--- template-parts/content-frontpage.php
+++ template-parts/content-frontpage.php
@@ -25,14 +25,56 @@
         <div class="entry-content frontpage-content">
             <?php
             the_content();
-
-            wp_link_pages( array(
-                'before' => '<div class="page-links">Pages:',
-                'after'  => '</div>',
-            ) );
             ?>
         </div><!-- .entry-content -->
         <?php
     endif;
     ?>
 </article><!-- #post-## -->
+
+<div class="separator prefix_1 suffix_1"><span class="grid_9"></span></div>
+
+<!-- Wicget column 1 - events widget -->
+<section class="widget calender_widget grid_5">
+    <h2 class="widget_title">Upcoming Trips & Events</h2>
+
+    <article class="calendar_entry">
+        <div class="date_stamp">
+            <span class="day">12</span>
+            <span class="month">Jun</span>
+        </div>
+        <h4>Trip: Stanage Edge</h4>
+        <p>Meet outside the RKC at 9am</p>
+    </article>
+
+    <article class="calendar_entry">
+        <div class="date_stamp">
+            <span class="day">29</span>
+            <span class="month">Oct</span>
+        </div>
+        <h4>Event: Freshers Fair</h4>
+        <p>9am, we'll be at the back of the sports tent</p>
+    </article>
+
+    <a href="" class="button view_all_more_button">
+        See All
+    </a>
+</section>
+<div class="grid_1">&nbsp;</div>
+<section class="widget social_widget grid_5">
+    <h2 class="widget_title">Latest Social Media Updates</h2>
+
+    <article class="social_entry">
+        <div class="icon_stamp fb">&nbsp;</div>
+        <p>YUMC crushed Lancaster at the bouldering comp last night, earning a huge 11.5 Roses [...]</p>
+    </article>
+
+    <article class="social_entry">
+        <div class="icon_stamp twitter">&nbsp;</div>
+        <p>YUMC crushed Lancaster at the bouldering comp last night, earning a huge 11.5 Roses [...]</p>
+    </article>
+
+    <a href="" class="button view_all_more_button">
+        View More
+    </a>
+</section>
