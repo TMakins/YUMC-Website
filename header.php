ee25e27c3a478409336388b2f90a2ee54e9ab0be
Toby Makins <toby@makins.org.uk>
2016-06-12 18:44:54 +0100

    Added custom header

--- header.php
+++ header.php
@@ -15,7 +15,11 @@
 	</head>
 	<body <?php body_class() ?>>
 		<header id="masthead" class="site-header" role="banner">
-            <img id="header-image" src="<?php echo get_template_directory_uri(); ?>/images/header-image.jpg" alt="Some mountains somewhere" />
+			<?php if ( get_header_image() ) : ?>
+					<img id="header-image" src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+			<?php else: ?>
+				<img id="header-image" src="<?php echo get_template_directory_uri(); ?>/images/header-image.jpg" alt="Some mountains somewhere" />
+			<?php endif; ?>
             <a href="<?php echo site_url(); ?>" title="YUMC Home">
                 <h1 id="logo"><?php bloginfo( "name" ); ?></h1>
             </a>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,12 +5,6 @@
  *  PHP: Setup custom header implementation
  */
 
-<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
-<?php endif; // End header image check. ?>
-
 /**
  * Set up the WordPress core custom header feature.
  *
